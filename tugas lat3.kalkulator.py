037ec594286251fdf9374e8401628e587368d8fd
salsafadhilah <37093417+salsafadhilah@users.noreply.github.com>
2018-04-17 05:04:25 -0700

    Add files via upload

---
+++ tugas lat3.kalkulator.py
@@ -0,0 +1,19 @@
+def aa():
+    print("tugas kalkulator")
+
+print"      KALKULATOR \n"
+print"=========================\n\n"
+
+def fungsi (nilai1,nilai2,tugas):
+    if tugas=="+":
+        hasil= nilai1+nilai2
+    elif tugas=="-":
+        hasil= nilai1-nilai2
+    elif tugas=="*":
+        hasil= nilai1*nilai2
+    elif tugas== "/":
+        hasil= nilai1/nilai2
+
+        return hasil
+    
+
